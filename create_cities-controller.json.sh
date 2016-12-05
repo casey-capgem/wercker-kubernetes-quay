9a80ee559d0eb84835af2bd3e105882215418df1
casey-capgem <casey.rogers@capgemini.com>
2016-12-05 15:15:24 +0000

    Update create_cities-controller.json.sh

--- create_cities-controller.json.sh
+++ create_cities-controller.json.sh
@@ -26,7 +26,7 @@ cat > cities-controller.json <<EOF
         "containers": [
           {
             "imagePullPolicy": "Always",
-            "image": "eu.gcr.io/analytical-rain-151012/c-test",
+            "image": "eu.gcr.io/analytical-rain-151012/c-test:${WERCKER_GIT_COMMIT}",
             "name": "cities",
             "ports": [
               {
